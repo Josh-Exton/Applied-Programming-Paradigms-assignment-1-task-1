2ad0b60c44afe0eb064bc4d5bdc324afe9c55ae9
Student <Student@1A-G1-WINDOWS>
2024-09-26 15:52:22 +0100

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+string CaesarCipher(string text, int val, int key)
+{
+    for (int i = 0; i < text.length(); i++)
+    {
+        if (val == 0)
+        {
+            text[i] = text[i] + key;
+        }
+        else if (val == 1)
+        {
+            text[i] = text[i] - key;
+        }
+    }
+
+    cout << text << "\n";
+    return text;
+}
+
+int main()
+{
+    string test1 = CaesarCipher("hello", 0, 1);
+    string test2 = CaesarCipher("ifmmp", 1, 1);
+    string test3 = CaesarCipher("abcxyz", 0, 2);
+    string test4 = CaesarCipher("cdezab", 1, 2);
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
